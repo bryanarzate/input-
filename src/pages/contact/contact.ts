2d8e0bf9e6293810e3d32210352e9e1439cfaab5
Bryan Arzate <bryanarzate@Bryans-MacBook-Pro.local>
2018-07-27 11:29:33 -0700

    rob

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController, AlertController, NavParams } from 'ionic-angular';
 import { PricePage } from '../price/price';
 
@@ -108,7 +108,7 @@ export class ContactPage {
   ]
 
   currentBanda;
-  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, public zone: NgZone) {
     this.currentBanda = this.bandas.filter((data) => {
       if(data.id === this.navParams.get('id')){
         return data
@@ -125,51 +125,58 @@ export class ContactPage {
   }
 
   getDaysOfMonth() {
-    this.daysInThisMonth = new Array();
-    this.daysInLastMonth = new Array();
-    this.daysInNextMonth = new Array();
-    this.currentMonth = this.monthNames[this.date.getMonth()];
-    this.currentYear = this.date.getFullYear();
-    if(this.date.getMonth() === new Date().getMonth()) {
-      this.currentDate = new Date().getDate();
-    } else {
-      this.currentDate = 999;
-    }
-
-    var firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
-    var prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
-    for(var i = prevNumOfDays-(firstDayThisMonth-1); i <= prevNumOfDays; i++) {
-      this.daysInLastMonth.push(i);
-    }
-
-    var thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDate();
-    for (var j = 0; j < thisNumOfDays; j++) {
-      this.daysInThisMonth.push(j+1);
-    }
-
-    var lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDay();
-    // var nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0).getDate();
-    for (var k = 0; k < (6-lastDayThisMonth); k++) {
-      this.daysInNextMonth.push(k+1);
-    }
-    var totalDays = this.daysInLastMonth.length+this.daysInThisMonth.length+this.daysInNextMonth.length;
-    if(totalDays<36) {
-      for(var l = (7-lastDayThisMonth); l < ((7-lastDayThisMonth)+7); l++) {
-        this.daysInNextMonth.push(l);
+    this.zone.run(() => {
+      this.daysInThisMonth = new Array();
+      this.daysInLastMonth = new Array();
+      this.daysInNextMonth = new Array();
+      this.currentMonth = this.monthNames[this.date.getMonth()];
+      this.currentYear = this.date.getFullYear();
+      if(this.date.getMonth() === new Date().getMonth()) {
+        this.currentDate = new Date().getDate();
+      } else {
+        this.currentDate = 999;
       }
-    }
+  
+      var firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
+      var prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
+      for(var i = prevNumOfDays-(firstDayThisMonth-1); i <= prevNumOfDays; i++) {
+        this.daysInLastMonth.push(i);
+      }
+  
+      var thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDate();
+      for (var j = 0; j < thisNumOfDays; j++) {
+        this.daysInThisMonth.push(j+1);
+      }
+  
+      var lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDay();
+      // var nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0).getDate();
+      for (var k = 0; k < (6-lastDayThisMonth); k++) {
+        this.daysInNextMonth.push(k+1);
+      }
+      var totalDays = this.daysInLastMonth.length+this.daysInThisMonth.length+this.daysInNextMonth.length;
+      if(totalDays<36) {
+        for(var l = (7-lastDayThisMonth); l < ((7-lastDayThisMonth)+7); l++) {
+          this.daysInNextMonth.push(l);
+        }
+      }
+  
+    })
+
+    console.log(this.currentMonth);
   }
 
   goToLastMonth() {
     console.log('last month pressed')
     this.date = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
     this.getDaysOfMonth();
+
   }
 
   goToNextMonth() {
     console.log('next month pressed');
     this.date = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0);
     this.getDaysOfMonth();
+
   }
 
   checkEvent(day) {
